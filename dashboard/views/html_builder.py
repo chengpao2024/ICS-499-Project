2e383fc2861baaa3b38b51ef43cfa20f072fc62c
cymetro2 <calvin.yang.2@my.metrostate.edu>
2026-03-26 17:24:31 -0500

    updated to navigate rental request to rental page

--- dashboard/views/html_builder.py
+++ dashboard/views/html_builder.py
@@ -186,21 +186,19 @@ class HtmlBuilder:
     @staticmethod
     def _rent_button_cell(aid: int, name_attr: str) -> str:
         return f"""
-          <td class="td-action">
-            <button class="btn-rent"
-                    data-id="{aid}" data-name="{name_attr}"
-                    onclick="openRentModal({aid}, '{name_attr}')">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                   viewBox="0 0 24 24" width="13" height="13"
-                   stroke-width="2" stroke="currentColor">
+        <td class="td-action">
+            <a href="/public/rental_create.php" class="btn-rent">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 24 24" width="13" height="13"
+                stroke-width="2" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round"
-                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383
-                         1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218
-                         c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0
-                         00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75
-                         0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0
-                         11-1.5 0 .75.75 0 011.5 0z"/>
-              </svg>
-              Request Rental
-            </button>
-          </td>"""
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383
+                        1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218
+                        c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0
+                        00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75
+                        0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0
+                        11-1.5 0 .75.75 0 011.5 0z"/>
+            </svg>
+            Request Rental
+            </a>
+        </td>"""
